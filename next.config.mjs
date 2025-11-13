5f43abe8873712b04d35ab5a60cb1a23da1be6e9
Veeti Nurmikoski <veetinurmikoski@Veeti-MacBook-Pro.local>
2025-11-13 13:01:21 +0200

    Pois Eslint on build ja turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb'
     }
   }
+}
 };
 export default nextConfig;
